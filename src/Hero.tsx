8d72d27380e9901a907dc3fd9d341b6483cdac8b
Avuo <46840244+Avuo@users.noreply.github.com>
2022-11-06 00:03:15 +0200

    Hero section

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import styles from './Hero.module.css';
-
+import React from "react";
+import styles from "./Hero.module.css";
 
 export function Hero() {
-    return(
-        <>
+  return (
+    <>
       <div className={styles.hero_wrapper}>
-      <div className={styles.hero_image}></div>
-      <div className={styles.hero_motto}> Snap! </div> 
+        <div className={styles.hero_image}></div>
+        <div className={styles.hero_motto}> Snap! It's in the Blockchain.</div>
+        <div className={styles.hero_motto2}> Forever.</div>
+        <div className={styles.hero_text}>
+          Orwello immortalizes your Discord messages. Invite Orwello to your
+          server and start minting.
+        </div>
+        <div>
+          <div className={styles.button_left}> &#129302; Invite Orwello</div>
+          <div className={styles.button_right}> Join the Community </div>
+        </div>
       </div>
-
-      </>
-    );
-  }
+    </>
+  );
+}
