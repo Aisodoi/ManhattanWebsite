8667241b7b23427fc41bcec508ea8c5703c3ee19
Joa Riski <joa.riski@aisodoi.com>
2022-11-06 08:00:56 +0200

    yes

--- src/pages/index/Banner.tsx
+++ src/pages/index/Banner.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import styles from './Banner.module.css';
+import { Link } from "react-router-dom";
 
 export function Banner() {
-    return (
-      <div className={styles.banner_render}>
-      <div className={styles.banner_profile_image_wrapper}>
-    <div className={styles.banner_profile_image}>
-      </div>
-      </div>
-    <div className={styles.banner_wrapper}>
-    <div className={styles.banner_header}>
-      Orwello
+  return (
+    <div className={styles.banner_render}>
+      <Link to={"/"}>
+        <div className={styles.banner_profile_image_wrapper}>
+          <div className={styles.banner_profile_image} />
+        </div>
+        <div className={styles.banner_wrapper}>
+          <div className={styles.banner_header}>
+            Orwello
+          </div>
+        </div>
+        <Link to={"/collection"}>
+          <div className={styles.collection}>
+            <div className={styles.navLink}>
+              Collection
+            </div>
+          </div>
+        </Link>
+      </Link>
     </div>
-      </div>
-      </div>
-  
-    );
-    }
+  );
+}
