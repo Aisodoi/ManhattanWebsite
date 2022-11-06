bb6c64e75e82361198d4127708dbef4d78aa0520
Joa Riski <joa.riski@aisodoi.com>
2022-11-06 06:49:06 +0200

    Add NFT minting capability

--- src/pages/mint/MintPage.tsx
+++ src/pages/mint/MintPage.tsx
@@ -1,11 +1,63 @@
 import { useParams } from "react-router-dom";
 import { Layout } from "../../layout/Layout";
 import React from "react";
-import { useIpfs } from "./hooks";
+import { useIpfs, useEth, useWeb3, useEthAcc, useContract } from "./hooks";
 import { DiscordMessage } from "./types";
 import { MessageRender } from "./MessageRender";
 import styles from "./MintPage.module.css";
 
+interface MintActionsProps {
+  messageId: string;
+  blobId: string;
+}
+export const MintActions: React.FC<MintActionsProps> = ({ messageId, blobId }) => {
+  const eth = useEth();
+  const web3 = useWeb3();
+  const contract = useContract(web3);
+  const { acc, refreshAcc } = useEthAcc(web3);
+  // const [acc, setAcc] = useState<string | undefined>(undefined);
+
+  function connect() {
+    if (!eth) return;
+    eth
+      .request({ method: "eth_requestAccounts" })
+      .then(refreshAcc)
+      .catch((error) => {
+        if (error.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          console.log("Please connect to MetaMask.");
+        } else {
+          console.error(error);
+        }
+      });
+  }
+
+  function mint() {
+    console.log(messageId);
+    contract.methods.safeMint(acc, messageId, blobId).send({
+      from: acc,
+      gasPrice: web3.utils.toHex(0),
+      gasLimit: web3.utils.toHex(5000000)
+    }, (err: any, res: any) => {
+      console.log(err);
+      console.log(res);
+    });
+  }
+
+  return (
+    <div>
+      {(acc !== undefined ? (
+        <button className={undefined} onClick={mint}>
+          Mint NFT
+        </button>
+      ) : (
+        <button className={undefined} onClick={connect}>
+          Connect wallet
+        </button>
+      ))}
+    </div>
+  );
+}
 
 export const MintPage = () => {
   const params = useParams<{ blobId: string }>();
@@ -23,6 +75,10 @@ export const MintPage = () => {
     return <Layout><p>{error.toString()}</p></Layout>
   }
 
+  if (!params.blobId) {
+    return <Layout><p>No data</p></Layout>
+  }
+
   if (!message) {
     return <Layout><p>No data</p></Layout>
   }
@@ -32,6 +88,12 @@ export const MintPage = () => {
       <div className={styles.container}>
         <div className={styles.messageContainer}>
           <MessageRender message={message} />
+        </div>
+        <div>
+          <MintActions
+            messageId={message.message.id}
+            blobId={params.blobId}
+          />
         </div>
       </div>
     </Layout>
--- src/pages/mint/hooks.ts
+++ src/pages/mint/hooks.ts
@@ -1,4 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import Web3 from "web3";
+import abi from "./abi.json";
+
 
 interface UseIPFS<T> {
   data?: T;
@@ -44,4 +47,41 @@ export function useIpfs<T>(blobId?: string): UseIPFS<T> {
     error: error,
     isLoading: isLoading,
   };
+}
+
+export const useEth = () => {
+  // const Web3 = require("web3");
+  // const web3 = new Web3("https://cloudflare-eth.com");
+  // web3.eth.getBlockNumber((error: any, result: any) => {
+  //   console.log(result)
+  // });
+  return window.ethereum;
+};
+
+export const useWeb3 = () => {
+  return new Web3(Web3.givenProvider || "https://cloudflare-eth.com");
+}
+
+export const useEthAcc = (web3: Web3) => {
+  const [acc, setAcc] = useState<string | undefined>(undefined);
+
+  const refreshAcc = useCallback(() => {
+    web3.eth.getAccounts().then(x => setAcc(!!x.length ? x[0] : undefined));
+  }, [web3]);
+
+  useEffect(() => {
+    refreshAcc();
+  }, [refreshAcc, web3]);
+
+  return {
+    acc,
+    refreshAcc,
+  };
+}
+
+export const useContract = (web3: Web3) => {
+  return new web3.eth.Contract(
+    abi as any,
+    "0x17a03ec1dF5Dc764f916D35AD047de003129a34F",
+  );
 }
