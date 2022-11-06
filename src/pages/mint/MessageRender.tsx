42e5722f9b8cc3a8fc09ec3447fa24a52ad8462b
Joa Riski <joa.riski@aisodoi.com>
2022-11-06 05:15:51 +0200

    Improve Discord message render

--- src/pages/mint/MessageRender.tsx
+++ src/pages/mint/MessageRender.tsx
@@ -11,8 +11,10 @@ export const MessageRender: React.FC<{ message: DiscordMessage }> = ({message}) 
         <img src={message.message.author.avatarUrl} className={styles.avatar} />
       </div>
       <div className={styles.bodyContainer}>
-        <div className={styles.header}>{message.message.author.username}</div>
-        <div className={styles.timestamp}>{message.message.editedTimestamp || message.message.createdTimestamp}</div>
+        <div className={styles.header}>
+          <div className={styles.author}>{message.message.author.username}</div>
+          <div className={styles.timestamp}>{message.message.editedTimestamp || message.message.createdTimestamp}</div>
+        </div>
         <div className={styles.messageBody}>{message.message.content}</div>
       </div>
     </div>
